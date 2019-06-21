dc404ad546766164ef6118b1595378f67d841dd1
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-06-21 08:49:06 -0400

    Add more examples to buttonGroup app

--- inst/examples/buttonGroup/app.R
+++ inst/examples/buttonGroup/app.R
@@ -25,12 +25,33 @@ ui <- fluidPage(
     multiple = TRUE
   ),
   tags$p(),
-  verbatimTextOutput("chosen_checkbox")
+  verbatimTextOutput("chosen_checkbox"),
+  tags$h4("Buttons with Style"),
+  shinyThings::buttonGroup(
+    inputId = "button_style",
+    options = button_options,
+    btn_class = paste0("btn-", c("primary", "success", "info", "warning", "danger")),
+    multiple = TRUE
+  ),
+  tags$p(),
+  verbatimTextOutput("chosen_style"),
+  tags$h4("Buttons with Initial Settings"),
+  shinyThings::buttonGroup(
+    inputId = "button_init",
+    options = button_options,
+    selected = c("eleven", "mike", "lucas"),
+    btn_class = "btn-default btn-xs",
+    multiple = TRUE
+  ),
+  tags$p(),
+  verbatimTextOutput("chosen_init")
 )
 
 server <- function(input, output) {
   output$chosen_radio <- renderPrint({ input$button_radio })
   output$chosen_checkbox <- renderPrint({ input$button_checkbox })
+  output$chosen_style <- renderPrint({ input$button_style })
+  output$chosen_init <- renderPrint({ input$button_init })
 }
 
 shinyApp(ui = ui, server = server)
