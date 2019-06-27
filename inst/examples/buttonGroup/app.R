3b4c42953a5efb609bc6e22b64ec4ad37af578d5
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-06-27 15:21:03 -0400

    example app now two-column

--- inst/examples/buttonGroup/app.R
+++ inst/examples/buttonGroup/app.R
@@ -9,72 +9,99 @@ button_choices <- c(
   "Lucas Sinclair" = "lucas"
 )
 
+two_col <- function(a, b) {
+  fluidRow(
+    div(class = "col-xs-12 col-sm-6", a),
+    div(class = "col-xs-12 col-sm-6", b)
+  )
+}
+
 ui <- function(request) {
   fluidPage(
     titlePanel("shinyThings Button Group"),
     id = "button_group_page",
-    tags$h4("Radio Buttons"),
-    shinyThings::buttonGroup(
-      inputId = "button_radio",
-      choices = button_choices
-    ),
-    tags$p(),
-    verbatimTextOutput("chosen_radio"),
 
-    tags$h4("Checkbox Buttons"),
-    shinyThings::buttonGroup(
-      inputId = "button_checkbox",
-      choices = button_choices,
-      multiple = TRUE
-    ),
-    tags$p(),
-    verbatimTextOutput("chosen_checkbox"),
+    # tags$h4("A regular button"),
+    # actionButton("button_regular", "Regular Button"),
 
-    tags$h4("Buttons with Style"),
-    shinyThings::buttonGroup(
-      inputId = "button_style",
-      choices = button_choices,
-      btn_class = paste0("btn-", c("primary", "success", "info", "warning", "danger")),
-      multiple = TRUE
-    ),
-    tags$p(),
-    verbatimTextOutput("chosen_style"),
-
-    tags$h4("Buttons with Initial Settings"),
-    shinyThings::buttonGroup(
-      inputId = "button_init",
-      choices = button_choices,
-      selected = c("eleven", "mike", "lucas"),
-      btn_class = "btn-default btn-sm",
-      multiple = TRUE
-    ),
-    tags$p(),
-    verbatimTextOutput("chosen_init"),
-
-    tags$h4("Buttons with icons"),
-    shinyThings::buttonGroup(
-      inputId = "button_icon",
-      choices = c("left", "center", "justify", "right"),
-      btn_icon = paste0("align-", c("left", "center", "justify", "right")),
-      multiple = FALSE
-    ),
-    tags$p(),
-    verbatimTextOutput("chosen_icon"),
-
-    tags$h4("Buttons with HTML"),
-    shinyThings::buttonGroup(
-      inputId = "button_html",
-      choices = c("bold", "italic", "underline", "strikethrough"),
-      choice_labels = list(
-        HTML("<strong>B</strong>"),
-        HTML("<i>I</i>"),
-        HTML("<span style='text-decoration: underline'>U</span>"),
-        HTML("<del>S</del>")
+    two_col(
+      tagList(
+        tags$h4("Radio Buttons"),
+        shinyThings::buttonGroup(
+          inputId = "button_radio",
+          choices = button_choices
+        ),
+        tags$p(),
+        verbatimTextOutput("chosen_radio")
       ),
-      multiple = FALSE
+      tagList(
+        tags$h4("Checkbox Buttons"),
+        shinyThings::buttonGroup(
+          inputId = "button_checkbox",
+          choices = button_choices,
+          multiple = TRUE
+        ),
+        tags$p(),
+        verbatimTextOutput("chosen_checkbox")
+      )
     ),
-    tags$p(),
-    verbatimTextOutput("chosen_html"),
+
+    two_col(
+      tagList(
+        tags$h4("Buttons with Style"),
+        shinyThings::buttonGroup(
+          inputId = "button_style",
+          choices = names(button_choices),
+          choice_labels = names(button_choices),
+          btn_class = paste0("btn-", c("primary", "success", "info", "warning", "danger")),
+          multiple = TRUE
+        ),
+        tags$p(),
+        verbatimTextOutput("chosen_style")
+      ),
+      tagList(
+        tags$h4("Buttons with Initial Settings"),
+        shinyThings::buttonGroup(
+          inputId = "button_init",
+          choices = button_choices,
+          selected = c("eleven", "mike", "lucas"),
+          btn_class = "btn-default btn-sm",
+          multiple = TRUE
+        ),
+        tags$p(),
+        verbatimTextOutput("chosen_init")
+      )
+    ),
+
+    two_col(
+      tagList(
+        tags$h4("Buttons with icons"),
+        shinyThings::buttonGroup(
+          inputId = "button_icon",
+          choices = c("left", "center", "justify", "right"),
+          btn_icon = paste0("align-", c("left", "center", "justify", "right")),
+          multiple = FALSE
+        ),
+        tags$p(),
+        verbatimTextOutput("chosen_icon")
+      ),
+      tagList(
+        tags$h4("Buttons with HTML"),
+        shinyThings::buttonGroup(
+          inputId = "button_html",
+          choices = c("bold", "italic", "underline", "strikethrough"),
+          choice_labels = list(
+            HTML("<strong>B</strong>"),
+            HTML("<i>I</i>"),
+            HTML("<span style='text-decoration: underline'>U</span>"),
+            HTML("<del>S</del>")
+          ),
+          multiple = TRUE
+        ),
+        tags$p(),
+        verbatimTextOutput("chosen_html")
+      )
+    ),
 
     bookmarkButton()
   )
